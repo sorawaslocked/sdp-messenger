ebdf560aa8004d41f67b9534ed20f0766c64d759
sora <133684349+sorawaslocked@users.noreply.github.com>
2024-11-05 20:09:27 +0500

    Initial

---
+++ src/main/java/com/sdp/sdpmessenger/SdpMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.sdp.sdpmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdpMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdp/sdpmessenger/SdpMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdp.sdpmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
