7b54d74de6c040798e1dc99e6d179d5557cf0a35
Dosymzhan <157879347+calmchipf@users.noreply.github.com>
2024-11-07 01:03:39 +0500

    Add RegisterValidator extension

--- src/main/java/com/sdp/sdpmessenger/authentication/validators/register/PasswordRegisterValidator.java
+++ src/main/java/com/sdp/sdpmessenger/authentication/validators/register/PasswordRegisterValidator.java
@@ -4,12 +4,23 @@ import com.sdp.sdpmessenger.authentication.RegisterRequest;
 import org.springframework.http.HttpStatus;
 import java.util.regex.Pattern;
 
-public class PasswordRegisterValidator {
+public class PasswordRegisterValidator extends RegisterValidator {
 
     private static final String PASSWORD_PATTERN =
             "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
+    @Override
+    public HttpStatus validate(RegisterRequest request) {
+        String password = request.getPassword();
+
+        if (!isPasswordValid(password)) {
+            return HttpStatus.BAD_REQUEST;  // Invalid password format
+        }
+
+        // If valid, proceed to the next validator in the chain
+        return validateNext(request);
+    }
 
     // Method that validates the password based on defined rules
     private boolean isPasswordValid(String password) {
